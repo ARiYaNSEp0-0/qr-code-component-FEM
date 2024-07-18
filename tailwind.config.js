fe767d693a8aaa5f891374a63b7277b64aa019f6
AriyanSep0_0 <ariansepahi825121314151617@gmail.com>
2024-07-18 21:04:56 +0330

    :tada: Initial tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          600: "#3685FF",
+          700: "#2C7DFA",
+          900: "#1F314F",
+        },
+        gray: {
+          300: "#D5E1EF",
+          600: "#7D889E",
+        },
+      },
+      fontFamily: {
+        sans: ["Outfit", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
